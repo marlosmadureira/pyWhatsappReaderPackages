dd224af64a40e648a23362c493dcf6e1882320f4
madureira <madureira_marlos@yahoo.com.br>
2024-02-23 23:43:11 -0300

    Prtt funcionando

--- pyPRTT.py
+++ pyPRTT.py
@@ -2,24 +2,43 @@ def message_logReader(message_log, DebugMode):
     if DebugMode:
         print(message_log)
 
-    data = {}
+    campos_desejados = ['Timestamp', 'Message Id', 'Sender', 'Recipients', 'Sender Ip', 'Sender Port', 'Sender Device',
+                        'Type', 'Message Style', 'Message Size']
 
-    fields = message_log.find_all("div", class_="div_table", style="font-weight: bold;")
+    # Lista para armazenar todos os registros
+    todos_os_registros = []
 
-    for field in fields:
-        # Tenta encontrar o nome do campo de uma maneira que exclua o valor
-        field_name_div = field.find("div", style="font-weight: bold; display:table;")
-        field_name_text = field_name_div.text.strip() if field_name_div else ""
-        # Se houver um valor associado diretamente, vamos removê-lo do nome do campo
-        field_value_div = field.find("div",
-                                     style="font-weight: normal; display:table-cell; padding: 2px; word-break: break-word; word-wrap: break-word !important;")
-        if field_value_div:
-            field_value = field_value_div.text.strip()
-            # Supondo que o valor sempre segue o nome do campo na mesma linha, podemos substituir o valor por '' para obter apenas o nome do campo
-            field_name = field_name_text.replace(field_value, '').strip()
-            data[field_name] = field_value
+    # Encontrar todos os blocos de mensagem
+    message_blocks = message_log.find_all("div", class_="div_table", style="font-weight: bold; display:table;")
 
-    print(data)
+    # Iterar sobre cada bloco de mensagem
+    for block in message_blocks:
+        # Dicionário para armazenar os dados de um registro
+        data = {}
+
+        # Encontrar todos os campos dentro de um bloco
+        fields = block.find_all("div", class_="div_table", style="font-weight: bold;")
+
+        # Iterar sobre cada campo e extrair informações
+        for field in fields:
+            field_name_div = field.find("div", style="font-weight: bold; display:table;")
+            field_name_text = field_name_div.text.strip() if field_name_div else ""
+
+            field_value_div = field.find("div",
+                                         style="font-weight: normal; display:table-cell; padding: 2px; word-break: break-word; word-wrap: break-word !important;")
+            if field_value_div:
+                field_value = field_value_div.text.strip()
+                field_name = field_name_text.replace(field_value, '').strip()
+                if field_name in campos_desejados:
+                    data[field_name] = field_value
+
+        if len(data) > 0:
+            # Adicionar o registro à lista
+            todos_os_registros.append(data)
+
+    # Print dos registros
+    for registro in todos_os_registros:
+        print(registro)
 
     if data is not None:
         return data

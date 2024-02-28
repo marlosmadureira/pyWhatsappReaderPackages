d3a6081f9abb68cc30828eb518f4f852fe0f15fd
marlosmadureira <71173817+marlosmadureira@users.noreply.github.com>
2024-02-28 15:43:36 -0300

    New Tag ncmec_reports

--- pyDados.py
+++ pyDados.py
@@ -182,7 +182,32 @@ def groups_infoReader(groups_info, fileName, DebugMode):
 def ncmec_reportsReader(ncmec_reports, fileName, DebugMode):  # SEM AMOSTRA PARA TESTAR
     print_color(f"\n=========================== PROCESSANDO NCMEC REPORTS ===========================", 32)
 
-    return None
+    if DebugMode:
+        print(f"{ncmec_reports}")
+
+    data = {}
+
+    fields = ncmec_reports.find_all("div", class_="div_table", style="font-weight: bold;")
+
+    for field in fields:
+        # Tenta encontrar o nome do campo de uma maneira que exclua o valor
+        field_name_div = field.find("div", style="font-weight: bold; display:table;")
+        field_name_text = field_name_div.text.strip() if field_name_div else ""
+        # Se houver um valor associado diretamente, vamos removê-lo do nome do campo
+        field_value_div = field.find("div",
+                                     style="font-weight: normal; display:table-cell; padding: 2px; word-break: break-word; word-wrap: break-word !important;")
+        if field_value_div:
+            field_value = field_value_div.text.strip()
+            # Supondo que o valor sempre segue o nome do campo na mesma linha, podemos substituir o valor por '' para obter apenas o nome do campo
+            field_name = field_name_text.replace(field_value, '').strip()
+            data[remover_espacos_regex(field_name)] = field_value
+
+    print(f"OUT {data}")
+
+    if data is not None:
+        return data
+    else:
+        return None
 
 
 def connection_infoReader(connection_info, fileName, DebugMode):  # SEM AMOSTRA PARA TESTAR

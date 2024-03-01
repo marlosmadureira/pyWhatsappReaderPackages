5d04999a78a07fac705ea12fc8cbce3c0e335b12
marlosmadureira <71173817+marlosmadureira@users.noreply.github.com>
2024-03-01 10:59:56 -0300

    Fix pasta log

--- pyMain.py
+++ pyMain.py
@@ -240,6 +240,8 @@ if __name__ == '__main__':
     checkFolder(DIRERROS)
     checkFolder(DIREXTRACAO)
 
+    checkFolder("log")
+
     observer = Observer()
     observer.schedule(MyHandler(), path=DIRNOVOS if DIRNOVOS else '.')
     observer.start()

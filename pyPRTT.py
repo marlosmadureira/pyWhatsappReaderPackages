35683050769959691f9d81f67f09d8b0ac3ef044
marlosmadureira <71173817+marlosmadureira@users.noreply.github.com>
2024-03-25 10:35:29 -0300

    Novo Padrão PRTT

--- pyPRTT.py
+++ pyPRTT.py
@@ -77,17 +77,16 @@ def message_logReader(message_log, fileName, DebugMode, Out):
         messages = []
 
         # Ignora a definição do log de mensagem e foca nos registros de mensagens
-        message_blocks = message_log.find_all('div', class_='div_table')[1:]  # Pula a descrição
+        message_blocks = message_log.find_all('div', class_='div_table outer')[1:]  # Pula a descrição
 
         for block in message_blocks:
             message_info = {}
-            detail_blocks = block.find_all('div', class_='div_table', recursive=True)
+            detail_blocks = block.find_all('div', class_='div_table outer', recursive=True)
 
             for detail_block in detail_blocks:
-                key_div = detail_block.find('div', style='font-weight: bold; display:table;')
+                key_div = detail_block.find('div', class_='div_table inner')
                 if key_div:
-                    value_div = key_div.find_next('div', style=lambda
-                        value: 'display:table-cell;' in value if value else False)
+                    value_div = key_div.find_next('div', class_=lambda value: 'most_inner' in value if value else False)
                     if value_div:
                         key_text = clean_html(
                             key_div.get_text(strip=True).replace(value_div.get_text(strip=True), '').strip())
@@ -113,6 +112,52 @@ def message_logReader(message_log, fileName, DebugMode, Out):
         return None
 
 
+# def message_logReader(message_log, fileName, DebugMode, Out):
+#     if message_log is not None:
+#
+#         if DebugMode:
+#             print(message_log)
+#
+#         messages = []
+#
+#         # Ignora a definição do log de mensagem e foca nos registros de mensagens
+#         message_blocks = message_log.find_all('div', class_='div_table')[1:]  # Pula a descrição
+#
+#         for block in message_blocks:
+#             message_info = {}
+#             detail_blocks = block.find_all('div', class_='div_table', recursive=True)
+#
+#             for detail_block in detail_blocks:
+#                 key_div = detail_block.find('div', style='font-weight: bold; display:table;')
+#                 if key_div:
+#                     value_div = key_div.find_next('div', style=lambda
+#                         value: 'display:table-cell;' in value if value else False)
+#                     if value_div:
+#                         key_text = clean_html(
+#                             key_div.get_text(strip=True).replace(value_div.get_text(strip=True), '').strip())
+#                         value_text = clean_html(value_div.get_text(strip=True))
+#
+#                         # Evita a sobreposição de chaves, adicionando valores com o mesmo nome de chave
+#                         if key_text != "Message":
+#                             message_info[remover_espacos_regex(key_text)] = value_text
+#
+#             if message_info and message_info not in messages:
+#                 messages.append(message_info)
+#
+#         if Out:
+#             print_color(f"\n=========================== PROCESSANDO MESSAGES LOGS ===========================", 32)
+#
+#             print(f"OUT {messages}")
+#
+#         if len(messages) > 0:
+#             return messages
+#         else:
+#             return None
+#     else:
+#         return None
+#
+
+
 def call_logsReader(call_log_div, fileName, DebugMode, Out):
     if call_log_div is not None:
 
@@ -121,18 +166,17 @@ def call_logsReader(call_log_div, fileName, DebugMode, Out):
 
         call_logs = []
 
-        call_blocks = call_log_div.find_all('div', class_='div_table')[1:]  # Pula a descrição dos logs de chamada
+        call_blocks = call_log_div.find_all('div', class_='div_table outer')[1:]  # Pula a descrição dos logs de chamada
 
         for block in call_blocks:
             call_info = {}
-            detail_blocks = block.find_all('div', class_='div_table', recursive=True)
+            detail_blocks = block.find_all('div', class_='div_table outer', recursive=True)
 
             for detail_block in detail_blocks:
-                key_div = detail_block.find('div', style='font-weight: bold; display:table;')
+                key_div = detail_block.find('div', class_='div_table inner')
                 if key_div:
                     # Procura pelo próximo div que corresponde ao valor, considerando qualquer estilo após "display:table-cell;"
-                    value_div = key_div.find_next('div', style=lambda
-                        value: 'display:table-cell;' in value if value else False)
+                    value_div = key_div.find_next('div', class_=lambda value: 'most_inner' in value if value else False)
                     if value_div:
                         key_text = clean_html(
                             key_div.get_text(strip=True).replace(value_div.get_text(strip=True), '').strip())
@@ -161,3 +205,53 @@ def call_logsReader(call_log_div, fileName, DebugMode, Out):
             return None
     else:
         return None
+
+
+# def call_logsReader(call_log_div, fileName, DebugMode, Out):
+#     if call_log_div is not None:
+#
+#         if DebugMode:
+#             print(call_log_div)
+#
+#         call_logs = []
+#
+#         call_blocks = call_log_div.find_all('div', class_='div_table')[1:]  # Pula a descrição dos logs de chamada
+#
+#         for block in call_blocks:
+#             call_info = {}
+#             detail_blocks = block.find_all('div', class_='div_table', recursive=True)
+#
+#             for detail_block in detail_blocks:
+#                 key_div = detail_block.find('div', style='font-weight: bold; display:table;')
+#                 if key_div:
+#                     # Procura pelo próximo div que corresponde ao valor, considerando qualquer estilo após "display:table-cell;"
+#                     value_div = key_div.find_next('div', style=lambda
+#                         value: 'display:table-cell;' in value if value else False)
+#                     if value_div:
+#                         key_text = clean_html(
+#                             key_div.get_text(strip=True).replace(value_div.get_text(strip=True), '').strip())
+#                         value_text = clean_html(value_div.get_text(strip=True))
+#                         # key_text = key_div.get_text(strip=True)
+#                         # value_text = " ".join(value_div.stripped_strings).replace('\n', ' ').replace('<br/>', '\n')
+#
+#                         # Trata a possibilidade de múltiplos eventos dentro de uma única chamada
+#                         if key_text != "Call":
+#                             if 'Call Id' in key_text or 'Call Creator' in key_text:
+#                                 call_info[remover_espacos_regex(key_text)] = value_text
+#                             elif 'Events' in key_text:
+#                                 call_info[remover_espacos_regex(key_text)] = getEvents(value_text)
+#
+#             if call_info and call_info not in call_logs:
+#                 call_logs.append(call_info)
+#
+#         if Out:
+#             print_color(f"\n=========================== PROCESSANDO CALL LOGS ===========================", 32)
+#
+#             print(f"OUT {call_logs}")
+#
+#         if len(call_logs) > 0:
+#             return call_logs
+#         else:
+#             return None
+#     else:
+#         return None

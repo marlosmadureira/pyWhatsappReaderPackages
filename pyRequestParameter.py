ebbf54ef7a3ce02f0eb38e9ed70197527e238193
madureira <madureira_marlos@yahoo.com.br>
2024-04-10 07:34:31 -0300

    New Padrão

--- pyRequestParameter.py
+++ pyRequestParameter.py
@@ -2,6 +2,10 @@ from pyBiblioteca import print_color, remover_espacos_regex, tipoHtml
 
 
 def requestReaderParameter(parameters, DebugMode, Out):
+    tag1 = 't o'
+    tag2 = 't i'
+    tag3 = 'm'
+
     if DebugMode:
         print(f"{parameters}")
 
@@ -9,14 +13,14 @@ def requestReaderParameter(parameters, DebugMode, Out):
 
     tipoHtml(parameters)
 
-    fields = parameters.find_all("div", class_="div_table outer")
+    fields = parameters.find_all("div", class_=f"{tag1}")
 
     for field in fields:
         # Tenta encontrar o nome do campo de uma maneira que exclua o valor
-        field_name_div = field.find("div", class_="div_table inner")
+        field_name_div = field.find("div", class_=f"{tag2}")
         field_name_text = field_name_div.text.strip() if field_name_div else ""
         # Se houver um valor associado diretamente, vamos removê-lo do nome do campo
-        field_value_div = field.find("div", class_="most_inner")
+        field_value_div = field.find("div", class_=f"{tag3}")
         if field_value_div:
             field_value = field_value_div.text.strip()
             # Supondo que o valor sempre segue o nome do campo na mesma linha, podemos substituir o valor por '' para obter apenas o nome do campo
@@ -31,6 +35,37 @@ def requestReaderParameter(parameters, DebugMode, Out):
         return data
     else:
         return None
+
+# def requestReaderParameter(parameters, DebugMode, Out):
+#     if DebugMode:
+#         print(f"{parameters}")
+#
+#     data = {}
+#
+#     tipoHtml(parameters)
+#
+#     fields = parameters.find_all("div", class_="div_table outer")
+#
+#     for field in fields:
+#         # Tenta encontrar o nome do campo de uma maneira que exclua o valor
+#         field_name_div = field.find("div", class_="div_table inner")
+#         field_name_text = field_name_div.text.strip() if field_name_div else ""
+#         # Se houver um valor associado diretamente, vamos removê-lo do nome do campo
+#         field_value_div = field.find("div", class_="most_inner")
+#         if field_value_div:
+#             field_value = field_value_div.text.strip()
+#             # Supondo que o valor sempre segue o nome do campo na mesma linha, podemos substituir o valor por '' para obter apenas o nome do campo
+#             field_name = field_name_text.replace(field_value, '').strip()
+#             data[remover_espacos_regex(field_name)] = field_value
+#
+#     if Out:
+#         print_color(f"\n=========================== PROCESSANDO REQUEST PARAMENTER ===========================", 32)
+#         print(f"OUT {data}")
+#
+#     if data is not None:
+#         return data
+#     else:
+#         return None
 
 # def requestReaderParameter(parameters, DebugMode, Out):
 #     if DebugMode:

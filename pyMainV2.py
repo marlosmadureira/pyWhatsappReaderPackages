3704143532b9f1f6b14905e969b43405a29a371f
madureira <madureira_marlos@yahoo.com.br>
2024-05-30 09:45:49 -0300

    Fix

--- pyMainV2.py
+++ pyMainV2.py
@@ -114,23 +114,37 @@ def readHeader(bsHtml):
     ncmec_reports_definition = bsHtml.find(text="Ncmec Reports Definition")
     ncmec_cybertip_numbers = bsHtml.find(text="NCMEC CyberTip Numbers")
 
-    service_info = service.find_next().text
-    internal_ticket_number_info = somentenumero(internal_ticket_number.find_next().text)
-    account_identifier_info = somentenumero(account_identifier.find_next().text)
-    account_type_info = account_type.find_next().text
-    generated_info = generated.find_next().text
-    date_range_info = date_range.find_next().text
-    ncmec_reports_definition_info = ncmec_reports_definition.find_next().text
-    ncmec_cybertip_numbers_info = ncmec_cybertip_numbers.find_next().text
+    if service:
+        service_info = service.find_next().text
+        header['service_info'] = service_info
 
-    header['service_info'] = service_info
-    header['internal_ticket_number_info'] = internal_ticket_number_info
-    header['account_identifier_info'] = account_identifier_info
-    header['account_type_info'] = account_type_info
-    header['generated_info'] = generated_info
-    header['date_range_info'] = date_range_info
-    header['ncmec_reports_definition_info'] = ncmec_reports_definition_info
-    header['ncmec_cybertip_numbers_info'] = ncmec_cybertip_numbers_info
+    if internal_ticket_number:
+        internal_ticket_number_info = somentenumero(internal_ticket_number.find_next().text)
+        header['internal_ticket_number_info'] = internal_ticket_number_info
+
+    if account_identifier:
+        account_identifier_info = somentenumero(account_identifier.find_next().text)
+        header['account_identifier_info'] = account_identifier_info
+
+    if account_type:
+        account_type_info = account_type.find_next().text
+        header['account_type_info'] = account_type_info
+
+    if generated:
+        generated_info = generated.find_next().text
+        header['generated_info'] = generated_info
+
+    if date_range:
+        date_range_info = date_range.find_next().text
+        header['date_range_info'] = date_range_info
+
+    if ncmec_reports_definition:
+        ncmec_reports_definition_info = ncmec_reports_definition.find_next().text
+        header['ncmec_reports_definition_info'] = ncmec_reports_definition_info
+
+    if ncmec_cybertip_numbers:
+        ncmec_cybertip_numbers_info = ncmec_cybertip_numbers.find_next().text
+        header['ncmec_cybertip_numbers_info'] = ncmec_cybertip_numbers_info
 
     print(f"{header}")
 
@@ -141,23 +155,24 @@ def readGroup(bsHtml):
     pictures = []
     picture_sections = bsHtml.find_all(text='Picture')
 
-    for section in picture_sections:
-        picture_data = {}
+    if picture_sections:
+        for section in picture_sections:
+            picture_data = {}
 
-        # Navegar para os próximos elementos para extrair os dados
-        linked_media_file = section.find_next(text='Linked Media File:').find_next().text.strip()
-        thumbnail = section.find_next(text='Thumbnail').find_next().text.strip()
-        picture_id = section.find_next(text='ID').find_next().text.strip()
-        creation_date = section.find_next(text='Creation').find_next().text.strip()
-        size = section.find_next(text='Size').find_next().text.strip()
+            # Navegar para os próximos elementos para extrair os dados
+            linked_media_file = section.find_next(text='Linked Media File:').find_next().text.strip()
+            thumbnail = section.find_next(text='Thumbnail').find_next().text.strip()
+            picture_id = section.find_next(text='ID').find_next().text.strip()
+            creation_date = section.find_next(text='Creation').find_next().text.strip()
+            size = section.find_next(text='Size').find_next().text.strip()
 
-        picture_data['Linked Media File'] = linked_media_file
-        picture_data['Thumbnail'] = thumbnail
-        picture_data['ID'] = picture_id
-        picture_data['Creation'] = creation_date
-        picture_data['Size'] = size
+            picture_data['Linked Media File'] = linked_media_file
+            picture_data['Thumbnail'] = thumbnail
+            picture_data['ID'] = picture_id
+            picture_data['Creation'] = creation_date
+            picture_data['Size'] = size
 
-        pictures.append(picture_data)
+            pictures.append(picture_data)
 
     # Exibir os dados extraídos
     for picture in pictures:
@@ -170,100 +185,120 @@ def readBook(bsHtml):
     # Função para extrair números de uma seção específica
     contacts = []
     sectionsSymmetric = bsHtml.find_all(text='Symmetric contacts')
-    for section in sectionsSymmetric:
-        currentSymmetric = section.find_next()
-        print(f"{currentSymmetric}")
+    if sectionsSymmetric:
+        for section in sectionsSymmetric:
+            currentSymmetric = section.find_next()
+            print(f"{currentSymmetric}")
 
     sectionsAsymmetric = bsHtml.find_all(text='Asymmetric contacts')
-    for section in sectionsAsymmetric:
-        currentAsymmetric = section.find_next()
-        print(f"{currentAsymmetric}")
+    if sectionsAsymmetric:
+        for section in sectionsAsymmetric:
+            currentAsymmetric = section.find_next()
+            print(f"{currentAsymmetric}")
 
 
 def readMessageLogs(bsHtml):
     # Encontrar todos os blocos que contêm a informação "Timestamp"
     print("\nMessage Log")
     message_blocks = bsHtml.find_all(text="Timestamp")
+    menssages = []
+    if message_blocks:
+        # Iterar sobre cada bloco e extrair as informações
+        for block in message_blocks:
+            menssage_data = {}
+            timestamp = block.find_next().text.strip()
+            message_id = block.find_next(text="Message Id").find_next().text.strip()
+            sender = block.find_next(text="Sender").find_next().text.strip()
+            recipients = block.find_next(text="Recipients").find_next().text.strip()
+            sender_ip = block.find_next(text="Sender Ip").find_next().text.strip()
+            sender_port = block.find_next(text="Sender Port").find_next().text.strip()
+            sender_device = block.find_next(text="Sender Device").find_next().text.strip()
+            msg_type = block.find_next(text="Type").find_next().text.strip()
+            message_style = block.find_next(text="Message Style").find_next().text.strip()
+            message_size = block.find_next(text="Message Size").find_next().text.strip()
 
-    # Iterar sobre cada bloco e extrair as informações
-    for block in message_blocks:
-        timestamp = block.find_next().text.strip()
-        message_id = block.find_next(text="Message Id").find_next().text.strip()
-        sender = block.find_next(text="Sender").find_next().text.strip()
-        recipients = block.find_next(text="Recipients").find_next().text.strip()
-        sender_ip = block.find_next(text="Sender Ip").find_next().text.strip()
-        sender_port = block.find_next(text="Sender Port").find_next().text.strip()
-        sender_device = block.find_next(text="Sender Device").find_next().text.strip()
-        msg_type = block.find_next(text="Type").find_next().text.strip()
-        message_style = block.find_next(text="Message Style").find_next().text.strip()
-        message_size = block.find_next(text="Message Size").find_next().text.strip()
+            # Imprimir as informações extraídas
+            print(f"Timestamp: {timestamp}")
+            print(f"Message Id: {message_id}")
+            print(f"Sender: {sender}")
+            print(f"Recipients: {recipients}")
+            print(f"Sender Ip: {sender_ip}")
+            print(f"Sender Port: {sender_port}")
+            print(f"Sender Device: {sender_device}")
+            print(f"Type: {msg_type}")
+            print(f"Message Style: {message_style}")
+            print(f"Message Size: {message_size}")
+            print("\n---\n")
 
-        # Imprimir as informações extraídas
-        print(f"Timestamp: {timestamp}")
-        print(f"Message Id: {message_id}")
-        print(f"Sender: {sender}")
-        print(f"Recipients: {recipients}")
-        print(f"Sender Ip: {sender_ip}")
-        print(f"Sender Port: {sender_port}")
-        print(f"Sender Device: {sender_device}")
-        print(f"Type: {msg_type}")
-        print(f"Message Style: {message_style}")
-        print(f"Message Size: {message_size}")
-        print("\n---\n")
+            # menssage_data['timestamp'] = timestamp
+            # menssage_data['message_id'] = message_id
+            # menssage_data['sender'] = sender
+            # menssage_data['recipients'] = recipients
+            # menssage_data['sender_ip'] = sender_ip
+            # menssage_data['sender_port'] = sender_port
+            # menssage_data['sender_device'] = sender_device
+            # menssage_data['msg_type'] = msg_type
+            # menssage_data['message_style'] = message_style
+            # menssage_data['message_size'] = message_size
+            #
+            # menssages.append(menssage_data)
+
 
 
 def readCallLogs(bsHtml):
     # Encontrar todos os blocos que contêm a informação "Call"
     print("\nCall Logs")
     call_blocks = bsHtml.find_all(text='Call Id')
-    calls = []
-    for call_block in call_blocks:
-        call_data = {}
 
-        # Encontrar o bloco pai que contém as informações da chamada
-        parent = call_block.find_parent()
+    if call_blocks:
+        calls = []
+        for call_block in call_blocks:
+            call_data = {}
 
-        call_data['Call Id'] = parent.find_next(text='Call Id').find_next().text.strip()
-        call_data['Call Creator'] = parent.find_next(text='Call Creator').find_next().text.strip()
+            # Encontrar o bloco pai que contém as informações da chamada
+            parent = call_block.find_parent()
 
-        events = []
+            call_data['Call Id'] = parent.find_next(text='Call Id').find_next().text.strip()
+            call_data['Call Creator'] = parent.find_next(text='Call Creator').find_next().text.strip()
 
-        # Percorrer os eventos dentro do bloco de chamada
-        current = parent.find_next(text='Events').find_next()
+            events = []
 
-        while current and current.text.strip() not in ['Call Id', 'Events']:
-            if current.text.strip() == 'Type':
-                event_data = {}
-                event_data['Type'] = current.find_next().text.strip()
-                event_data['Timestamp'] = current.find_next(text='Timestamp').find_next().text.strip()
-                event_data['From'] = current.find_next(text='From').find_next().text.strip()
-                event_data['To'] = current.find_next(text='To').find_next().text.strip()
-                event_data['From Ip'] = current.find_next(text='From Ip').find_next().text.strip()
-                event_data['From Port'] = current.find_next(text='From Port').find_next().text.strip()
-                event_data['Media Type'] = current.find_next(
-                    text='Media Type').find_next().text.strip() if current.find_next(
-                    text='Media Type') else 'N/A'
-                events.append(event_data)
-                current = current.find_next(text='Type')
-            else:
-                current = current.find_next()
+            # Percorrer os eventos dentro do bloco de chamada
+            current = parent.find_next(text='Events').find_next()
 
-        call_data['Events'] = events
-        calls.append(call_data)
+            while current and current.text.strip() not in ['Call Id', 'Events']:
+                if current.text.strip() == 'Type':
+                    event_data = {}
+                    event_data['Type'] = current.find_next().text.strip()
+                    event_data['Timestamp'] = current.find_next(text='Timestamp').find_next().text.strip()
+                    event_data['From'] = current.find_next(text='From').find_next().text.strip()
+                    event_data['To'] = current.find_next(text='To').find_next().text.strip()
+                    event_data['From Ip'] = current.find_next(text='From Ip').find_next().text.strip()
+                    event_data['From Port'] = current.find_next(text='From Port').find_next().text.strip()
+                    event_data['Media Type'] = current.find_next(
+                        text='Media Type').find_next().text.strip() if current.find_next(
+                        text='Media Type') else 'N/A'
+                    events.append(event_data)
+                    current = current.find_next(text='Type')
+                else:
+                    current = current.find_next()
 
-    # Exibir os resultados
-    for call in calls:
-        print(f"Call Id: {call['Call Id']}")
-        print(f"Call Creator: {call['Call Creator']}")
-        for event in call['Events']:
-            print(f"  Event Type: {event['Type']}")
-            print(f"  Timestamp: {event['Timestamp']}")
-            print(f"  From: {event['From']}")
-            print(f"  To: {event['To']}")
-            print(f"  From Ip: {event['From Ip']}")
-            print(f"  From Port: {event['From Port']}")
-            print(f"  Media Type: {event['Media Type']}")
-        print()
+            call_data['Events'] = events
+            calls.append(call_data)
+
+        # Exibir os resultados
+        for call in calls:
+            print(f"Call Id: {call['Call Id']}")
+            print(f"Call Creator: {call['Call Creator']}")
+            for event in call['Events']:
+                print(f"  Event Type: {event['Type']}")
+                print(f"  Timestamp: {event['Timestamp']}")
+                print(f"  From: {event['From']}")
+                print(f"  To: {event['To']}")
+                print(f"  From Ip: {event['From Ip']}")
+                print(f"  From Port: {event['From Port']}")
+                print(f"  Media Type: {event['Media Type']}")
+            print()
 
 
 if __name__ == '__main__':

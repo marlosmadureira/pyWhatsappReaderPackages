7bb0b34bb6cadc5a44fdfcaae44c2434d457fe59
marlosmadureira <71173817+marlosmadureira@users.noreply.github.com>
2024-06-07 13:03:29 -0300

    Logic parse_dynamic_sentence_books

--- pyMainV3.py
+++ pyMainV3.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/python3
+import json
 import os
+import re
 
 from dotenv import load_dotenv
 from datetime import datetime
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
-from pyBibliotecaV3 import checkFolder, StatusServidor, printTimeData, countdown, printDebug, unzipBase, parse_dynamic_sentence_parameters, \
-    print_color, parsetHTLMFileString, grava_log, getUnidadeFileName, removeFolderFiles, delete_log
+from pyBibliotecaV3 import checkFolder, StatusServidor, printTimeData, countdown, printDebug, unzipBase, print_color, \
+    parsetHTLMFileString, grava_log, getUnidadeFileName, removeFolderFiles, delete_log
 from pyPostgresql import find_unidade_postgres
 
 # Configs
@@ -57,11 +59,11 @@ class MyHandler(PatternMatchingEventHandler):
 
                 NomeUnidade = find_unidade_postgres(Unidade)
 
-                print(f"\n{bsHtml}")
+                parsed_json_parameters = parse_dynamic_sentence_parameters(bsHtml)
+                print(f"\n{parsed_json_parameters}")
 
-                parsed_json = parse_dynamic_sentence_parameters(bsHtml)
-
-                print(f"\n{parsed_json}")
+                parsed_array_books = parse_dynamic_sentence_books(bsHtml)
+                print(f"\n{parsed_array_books}")
 
             else:
                 print_color(f"Erro Arquivo Contém Index: {fileName} Unidade: {Unidade}", 31)
@@ -109,6 +111,68 @@ class MyHandler(PatternMatchingEventHandler):
             print_color(f"\n{event.src_path} foi movido/renomeado para {event.dest_path}!", 36)
         except Exception as inst:
             pass
+
+
+def parse_dynamic_sentence_parameters(sentence):
+    # Expressões regulares para capturar os diferentes campos
+    patterns = {
+        "Service": r"Service(\w+)",
+        "Internal Ticket Number": r"Internal Ticket Number(\d+)",
+        "Account Identifier": r"Account Identifier(\+\d+)",
+        "Account Type": r"Account Type(\w+)",
+        "User Generated": r"User Generated(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} UTC)",
+        "Date Range": r"Date Range(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} UTC)"
+    }
+
+    # Dicionário para armazenar os resultados
+    result = {}
+
+    # Iterar sobre os padrões e encontrar as correspondências
+    for key, pattern in patterns.items():
+        match = re.search(pattern, sentence)
+        if match:
+            result[key] = match.group(1)
+
+    if len(result) > 0:
+        print("\nRequest Parameters")
+        return json.dumps(result, indent=4)
+    else:
+        return None
+
+
+def parse_dynamic_sentence_books(sentence):
+    allRegistros = []
+
+    data = {}
+
+    # Expressão regular para capturar números de telefone
+    phone_number_pattern = r"\b\d{11,}\b"
+
+    # Dividir a frase em seções "Symmetric" e "Asymmetric"
+    symmetric_section = re.search(r"Symmetric contacts\d+ Total ([\d\s]+)", sentence)
+    asymmetric_section = re.search(r"Asymmetric contacts\d+ Total ([\d\s]+)", sentence)
+
+    symmetric_numbers = []
+    asymmetric_numbers = []
+
+    # Extrair números de telefone da seção "Symmetric"
+    if symmetric_section:
+        symmetric_numbers = re.findall(phone_number_pattern, symmetric_section.group(1))
+
+    # Extrair números de telefone da seção "Asymmetric"
+    if asymmetric_section:
+        asymmetric_numbers = re.findall(phone_number_pattern, asymmetric_section.group(1))
+
+    data['Symmetric'] = symmetric_numbers
+    data['Asymmetric'] = asymmetric_numbers
+
+    allRegistros.append(data)
+
+    if len(data['Symmetric']) > 0 or len(data['Asymmetric']) > 0:
+        print("\nAddress Book Info")
+        return allRegistros
+    else:
+        return None
 
 
 if __name__ == '__main__':

83867608524e98c1f8286b931e13dc93a37ab34a
inteligencia <marlos.madureira@gmail.com>
2024-09-30 08:21:16 -0300

    Fix

--- pyGravandoDados.py
+++ pyGravandoDados.py
@@ -10,9 +10,6 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 
-APILINK = os.getenv("APILINK")
-APITOKEN = os.getenv("APITOKEN")
-
 executaSql = True
 PrintSql = True
 

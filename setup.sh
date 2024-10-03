7bb782047b5e98f3343a514c3c91398b73cdf659
madureira <madureira_marlos@yahoo.com.br>
2024-10-03 07:18:43 -0300

    Fix Lista Processamento

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 SCRIPT_DIR="/opt/pyWhatsappReaderPackages"
 
 # NOME DA PRINCIPAL DA APLICAÇÃO
-SCRIPT_NAME="pyMain.py"
+SCRIPT_NAME="pyMainV5.py"
 
 # NO DO SERVIÇO
 SCRIPT_SERVICE="pyWhatsappReaderPackages"
